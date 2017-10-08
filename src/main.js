c58aef86b3e87fa729fb5f248b4d2917176ed40f
liqinfei <526700468@qq.com>
2017-10-08 21:07:10 +0800

    two

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
 import axios from 'axios'
+Vue.prototype.$http = axios
+
 import "babel-polyfill"
+
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.config.productionTip = false
-Vue.prototype.$http = axios
 Vue.use(Element)
+
+import Vuex from 'vuex'
+import store from './vuex/store'
+ Vue.use(Vuex)
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
